c5ceb163b4b556a38db4d72ba17fa7a3622a6e03
amithshibu <amithshibu2002@gmail.com>
2022-12-06 12:35:25 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React , {useState}from 'react'
+import Button from './Components/Button'
+import './App.css'
+
+function App() {
+
+  const [count , setCount] = useState(0)
+
+  const updateCount = sign => {
+    let c = count
+    if(sign === "+") setCount(c+1)
+    else if(sign === "-") setCount(c-1)
+    else if (sign === "Reset") setCount(0)
+  }
+  return (
+  
+    <div className = "app">
+      <div className = "item1">
+        <Button sign = "+" updateCount = {updateCount}/>
+        <div className = "count">
+          {count}
+        </div>
+        <Button sign = "-" updateCount = {updateCount}/>
+      </div>
+      <br/>
+      <br/>
+      <div className = "item2">
+        <Button sign = "Reset" updateCount = {updateCount}/>
+      </div>
+    </div>
+  )
+}
+
+export default App
